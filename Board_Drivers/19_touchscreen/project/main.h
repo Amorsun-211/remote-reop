c62cb4de76584f49dbf98d80f19cb7e47386cbc8
Amorsun-211 <73333901+Amorsun-211@users.noreply.github.com>
2024-10-02 22:22:36 +0800

    Delete Board_Drivers directory

--- Board_Drivers/19_touchscreen/project/main.h
+++
@@ -1,5 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-#include "imx6u.h"
-
-#endif
--- Board_Drivers/8_clk/project/main.h
+++
@@ -1,8 +0,0 @@
-#ifndef _MAIN_H
-#define _MAIN_H
-#include "fsl_iomuxc.h"
-#include "MCIMX6Y2.h"
-#include "fsl_iomuxc.h"
-#include "cc.h"
-
-#endif
